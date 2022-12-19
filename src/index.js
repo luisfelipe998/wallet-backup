b01b7e997bd43ecdfda4b6324759dc401a56b2c5
Luis <luis.borsoi@sap.com>
2022-12-19 14:33:06 -0300

    add test cron schedule

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const mail = require('./mail');
 const fs = require('fs');
 
 async function main() {
-    fs.writeFileSync('./test.txt', 'heeellloooo');
+    fs.writeFileSync('./test.txt', new Date().toISOString());
     console.log('HEEELLLLLOOOOOO');
     mail.sendMail();
 }
